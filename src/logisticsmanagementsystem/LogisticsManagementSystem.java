f5541d7873e4df3a537fce5a9316569b7e2c5f0f
HP <HP@DESKTOP-8U91F8Q>
2025-10-21 00:47:55 +0530

    Added printMenue() method

--- src/logisticsmanagementsystem/LogisticsManagementSystem.java
+++ src/logisticsmanagementsystem/LogisticsManagementSystem.java
@@ -9,12 +9,70 @@ import java.util.Scanner;
  * @author HP
  */
 public class LogisticsManagementSystem {
+    
+    static Scanner input = new Scanner(System.in);
+    
+    //displaying menue
+    static void printMenue(){
+        System.out.println("\n==============================================");
+        System.out.println("     LOGISTICS MANAGEMENT SYSTEM      ");
+        System.out.println("==============================================");
+        System.out.println("1. Manage Cities");
+        System.out.println("2. Manage Distances");
+        System.out.println("3. Manage Vehicles");
+        System.out.println("4. Handle Deliveries");
+        System.out.println("5. Reports");
+        System.out.println("6. Exit");
+        System.out.print("Enter your choice: ");
+        
+    }
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        int choice;
+        do{
+            printMenue();
+            choice = input.nextInt();
+             
+            switch (choice) {
+                case 1:
+                    manageCities();
+                    break;
+                case 2:
+                    manageDistances();
+                    break;
+                case 3:
+                    manageVehicles();
+                    break;
+                case 4:
+                    handleDeliveries();
+                    break;
+                case 5:
+                    showReports();
+                    break;
+                case 6:
+                    System.out.println("Exiting");
+                    break;
+                default:
+                    System.out.println("Invalid choice");
+            }
+        } while (choice != 6);    
+    }        
+        
+    static void manageCities(){
+        
+ 
+              
+        
+        
+        
+    }
+    static void manageDistances(){
+    }
+    static void manageVehicles(){
+    }
+    static void handleDeliveries(){
+    }
+    static void showReports(){
     }
     
 }
