3d53419e2226bc9d677892851b71b6362587d84d
HP <HP@DESKTOP-8U91F8Q>
2025-10-21 14:03:09 +0530

    Added managecitiies() method with the relavant global variables

--- src/logisticsmanagementsystem/LogisticsManagementSystem.java
+++ src/logisticsmanagementsystem/LogisticsManagementSystem.java
@@ -12,6 +12,10 @@ public class LogisticsManagementSystem {
     
     
     static Scanner input = new Scanner(System.in);
+    static String[] cities = new String[30];
+    static int cityCount = 0;
+
+   
     //displaying menue
     static void printMenue(){
         System.out.println("\n==============================================");
@@ -26,7 +30,7 @@ public class LogisticsManagementSystem {
         System.out.print("Enter your choice: ");
         
     }
-
+    //main method
     public static void main(String[] args) {
         int choice;
         do{
@@ -58,11 +62,95 @@ public class LogisticsManagementSystem {
         } while (choice != 6);    
     }        
     
+    //city management
     static void manageCities(){
-       
+        System.out.println("\n--Manage Cities--");
+        System.out.println("1.Add a city");
+        System.out.println("2.Rename a city");
+        System.out.println("3.Remove a city");
+        System.out.print("Enter your choice: ");
+        int cityChoice = input.nextInt();
+        input.nextLine(); 
+        
+        switch (cityChoice){
+            case 1->addcity(); //caling addcity() method
+            case 2->renamecity(); //caling  renamecity() method
+            case 3->removecity(); //calling removecity() method    
+            default->System.out.println("Invalid choice");    
+                    
+        }
     }
     
+    static void addcity(){  //adding a new city
+        if (cityCount >= 30) {
+            System.out.println("City limit exceeded");
+            return;
+        }
+        System.out.print("Enter the city name:");
+        String newCityName=input.nextLine();
+        cities[cityCount]=newCityName;
+        cityCount++; //increasing counter
+        
+        System.out.println("New city "+newCityName+" added");
+        
+    }
+    
+    static void renamecity(){  //renameing a city
+        if (cityCount == 0) {  //checking whether if there are cities to rename
+            System.out.println("No cities added yet");
+            return;
+        }
+        
+        for (int i = 0; i < cityCount; i++) {
+            System.out.println(i + ") " + cities[i]); //List of the cities
+        }
+        
+        System.out.print("Enter index of the city to rename: ");
+        int index = input.nextInt();
+        input.nextLine();
+        
+        if (index >= 0 && index < cityCount) {
+            System.out.print("Enter new city name: ");
+            cities[index] = input.nextLine(); //storing renamed city
+            System.out.println(" City having index "+index+ "renamed to "+ cities[index]);
+        }    
+        else {
+            System.out.println("Invalid city index");
+        }
+        
+    }
+    
+    
+    static void removecity(){ //removeing a city
+        if (cityCount == 0) { //checking whther is there any cities added to remove
+            System.out.println("No cities added yet");
+            return;
+        }
+        
+        for (int i = 0; i < cityCount; i++) {
+            System.out.println(i + " " + cities[i]); //List of the cities
+            
+        }
+        
+        System.out.print("Enter index of the city to remove: ");
+        int index = input.nextInt();
+        
+        if (index >= 0 && index < cityCount) {
+            for (int i = index; i < cityCount - 1; i++) {  //shifting cities to the left after removing relavant city
+                cities[i] = cities[i + 1];
+            }
+            cityCount--;  //decreasing city counter
+            System.out.println(cities[index]+"city removed");
+        } 
+        else {
+            System.out.println("Invalid city index");
+        }
+    } 
+    
+    
     static void manageDistances(){
+        
+                
        
     }
     static void manageVehicles(){
@@ -72,4 +160,5 @@ public class LogisticsManagementSystem {
     static void showReports(){
     }
     
+
 }
